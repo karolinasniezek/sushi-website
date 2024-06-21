5d649d797e5816941330c7df9aede4a3a738430d
Karolina Śnieżek <karolina.sniezek@xebia.com>
2024-06-21 23:30:48 +0200

    add sections

--- sushi/js/script.js
+++ sushi/js/script.js
@@ -8,7 +8,7 @@ import sushi10 from "../assets/sushi-10.png";
 import AOS from "aos";
 import "aos/dist/aos.css";
 
-// init AOS animation
+// AOS animation
 AOS.init({
   duration: 1000,
   offset: 100,
